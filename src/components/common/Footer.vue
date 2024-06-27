03c8c0726c3735919092c8d2eb0a6894539175f0
Lenovo@Daniel <danielsandy434@gmail.com>
2024-06-27 22:41:48 +0700

    Perubahan ke-2

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -51,7 +51,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+                <div class="px-4 py-4 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
                     <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2024 <a
                             href="https://flowbite.com/">Si Bolang</a>. All Rights Reserved.
                     </span>
